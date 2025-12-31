2bb4e70d1a761aefd91dd75d28a0455df9a50743
DiligentViking <acore-n@proton.me>
2025-12-31 14:44:43 -0600

    Add base hashmap factory

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 A TOP practice project to master the quintessential *hashmap*.
 
 ![HashMap](./src/assets/images/hashmap-2453269496.png)
+Pretty handy, don't you think?
---
+++ src/scripts/index.js
@@ -0,0 +1,22 @@
+function HashMap() {
+  const loadFactor = 0.75;
+  let capacity = 16;
+
+  return {
+    hash(key) {
+      let hashCode = 0;
+
+      const primeNumber = 31;
+      for (let i = 0; i < key.length; i++) {
+        hashCode += primeNumber * hashCode + key.charCodeAt(i);
+      }
+
+      return hashCode;
+    },
+    
+  }
+}
+
+
+const hashMap = HashMap();
+
